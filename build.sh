36e22108ec7854e51ed77a1f14c999e9dd134161
Leizjx <lezidevwizard@gmail.com>
2025-05-21 16:19:40 +0700

    Fix WSGI configuration and Python path

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
+# Add the project directory to PYTHONPATH
+export PYTHONPATH=$PYTHONPATH:$(pwd)/phone_store
+
 # Collect static files
 python phone_store/manage.py collectstatic --no-input
 
