3a91902f68d7a8b0a8c2a23dcb09c6ee9e79f346
Leizjx <lezidevwizard@gmail.com>
2025-05-21 16:38:18 +0700

    Configure for Render deployment

--- build.sh
+++ build.sh
@@ -5,14 +5,8 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
-# Navigate to project directory
-cd phone_store
-
-# Set Python path
-export PYTHONPATH=$PYTHONPATH:$(pwd)
-
 # Collect static files
-python manage.py collectstatic --no-input
+python phone_store/manage.py collectstatic --no-input
 
 # Run migrations
-python manage.py migrate
+python phone_store/manage.py migrate
