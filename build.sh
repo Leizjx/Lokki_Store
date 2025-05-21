2264c855989217aaa7d57ecca348d1436788301e
Leizjx <lezidevwizard@gmail.com>
2025-05-21 16:10:10 +0700

    Fix directory structure for Render deployment

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
+# Navigate to the correct directory containing manage.py
+cd store
+
 # Collect static files
 python manage.py collectstatic --no-input
 
