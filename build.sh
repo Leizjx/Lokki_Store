d33eebfb45dfcff057f71146402410857a68be4c
Leizjx <lezidevwizard@gmail.com>
2025-05-21 17:12:53 +0700

    update render

--- build.sh
+++ build.sh
@@ -1,12 +1,8 @@
+# filepath: d:\ShopOnline\build.sh
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
-# Install dependencies
 pip install -r requirements.txt
 
-# Apply database migrations
-python manage.py migrate
-
-# Collect static files
-python manage.py collectstatic --no-input
+python phone_store/manage.py migrate
+python phone_store/manage.py collectstatic --no-input
