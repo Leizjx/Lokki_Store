723e2815e657fd8169b48a259e5a0dc12fa6d07e
Leizjx <lezidevwizard@gmail.com>
2025-05-21 18:16:50 +0700

    update render

--- phone_store/run_local.py
+++
@@ -1,6 +0,0 @@
-from waitress import serve
-from phone_store.wsgi import application
-
-if __name__ == '__main__':
-    print('Starting server on http://localhost:8000')
-    serve(application, host='localhost', port=8000)
