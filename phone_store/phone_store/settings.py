7558ee92a8f8844713eaa03d0ddc03f5effdee4a
nhantrinh <nhantrinh989@gmail.com>
2025-05-24 08:20:50 +0700

    Update settings.py

--- phone_store/phone_store/settings.py
+++ phone_store/phone_store/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-)u-i=0urz2wu72c37pouk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.render.com']
+ALLOWED_HOSTS = ['nhan1111.pythonanywhere.com']
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
